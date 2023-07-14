2154a1201bdb298fe15c663e5d4beebc6c1c9f35
tuannm6 <tuannm6@tng-holdings.vn>
2023-07-14 11:51:25 +0700

    first commit

---
+++ js/js01.js
@@ -0,0 +1,8 @@
+var james = $('#bond');
+var right = function () {
+    james.animate({left: '5px'}, 600, left);
+};
+var left = function () {
+    james.animate({left: '0px'}, 600, right);
+};
+right();
